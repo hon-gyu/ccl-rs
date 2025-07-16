25d5b4a933cc7f419f12c3e6629e146fd046c6d9
hongyu <hongyu@imandra.ai>
2025-07-16 23:41:11 +0100

    refactor: aggregate string pp utils

--- src/parser.rs
+++ src/parser.rs
@@ -1,21 +1,19 @@
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fmt::Display;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct KeyVal {
-    pub key: String,
-    pub value: String,
-}
-
-impl KeyVal {
-    pub fn new(key: String, value: String) -> Self {
-        Self {
-            key: key.trim().to_string(),
-            value: value.trim().to_string(),
-        }
+mod string_utils {
+    /// Indent a string by a given number of spaces for each line
+    pub fn indent(s: &str, indent: usize) -> String {
+        let indent_str = " ".repeat(indent);
+        s.lines()
+            .map(|line| format!("{}{}", indent_str, line))
+            .collect::<Vec<String>>()
+            .join("\n")
     }
 
-    fn dedent<'a>(lines: &[&'a str]) -> Vec<&'a str> {
+    /// Dedent a vector of strings by the minimum indentation
+    pub fn dedent<'a>(lines: &[&'a str]) -> Vec<&'a str> {
         let min_indent = lines
             .iter()
             .map(|line| line.len() - line.trim_start().len())
@@ -31,6 +29,38 @@ impl KeyVal {
         lines.iter().map(|line| &line[min_indent..]).collect()
     }
 
+    pub const BOX_DRAWING_CHARS: (&str, &str, &str, &str, &str, &str) =
+        ("┌", "┐", "┘", "└", "─", "│");
+
+    /// Add a box around a string
+    pub fn add_box(s: &str) -> String {
+        let (
+            top_left,
+            top_right,
+            bottom_right,
+            bottom_left,
+            _horizontal,
+            vertical,
+        ) = BOX_DRAWING_CHARS;
+        let lines = s.lines().collect::<Vec<&str>>();
+        let max_len = lines.iter().map(|line| line.len()).max().unwrap();
+        let mut result = String::new();
+        result.push_str(top_left);
+        result.push_str(&"─".repeat(max_len));
+        result.push_str(top_right);
+        result.push_str("\n");
+        for line in lines {
+            result.push_str(format!("{}{}", vertical, line).as_str());
+            let pad = " ".repeat(max_len - line.len());
+            result.push_str(format!("{}{}", pad, vertical).as_str());
+            result.push_str("\n");
+        }
+        result.push_str(bottom_left);
+        result.push_str(&"─".repeat(max_len));
+        result.push_str(bottom_right);
+        result
+    }
+}
     /// Parse a string into a vector of KeyVals by indentation
     pub fn parse_top_level(data: &str) -> Result<Vec<Self>, String> {
         let mut key_vals = Vec::new();
