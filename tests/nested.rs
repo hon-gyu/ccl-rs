46ea199cc7360c312019d23164fd072b3d45c457
hongyu <hongyu@imandra.ai>
2025-07-20 20:34:45 +0100

    wip: extra level in tree?

--- tests/nested.rs
+++ tests/nested.rs
@@ -2,6 +2,7 @@
 /// Extracted from vendor/ccl/test/test_parser/test_nested.ml
 
 use ccl_rs::key_val::KeyVal;
+use ccl_rs::parser::CCL;
 
 #[cfg(test)]
 mod tests {
@@ -75,4 +76,143 @@ key =
         let result = KeyVal::parse(config).unwrap();
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn test_extra_equation() {
+        let config = r#"
+        ports =
+            = 8000
+            = 8001
+            = 8002
+        "#;
+        let result = KeyVal::parse(config).unwrap();
+        insta::assert_debug_snapshot!(result, @r#"
+        [
+            KeyVal {
+                key: "ports",
+                value: "\n            = 8000\n            = 8001\n            = 8002",
+            },
+        ]
+        "#);
+        let key_val_tree = KeyVal::parse_flat_to_tree(&result);
+        insta::assert_debug_snapshot!(key_val_tree, @r#"
+        {
+            "ports": [
+                Tree(
+                    {
+                        "": [
+                            Leaf(
+                                "8000",
+                            ),
+                            Leaf(
+                                "8001",
+                            ),
+                            Leaf(
+                                "8002",
+                            ),
+                        ],
+                    },
+                ),
+            ],
+        }
+        "#);
+        let ccl = CCL::parse(result);
+        insta::assert_debug_snapshot!(ccl, @r#"
+        CCL(
+            {
+                "ports": CCL(
+                    {
+                        "": CCL(
+                            {
+                                "8000": CCL(
+                                    {},
+                                ),
+                                "8001": CCL(
+                                    {},
+                                ),
+                                "8002": CCL(
+                                    {},
+                                ),
+                            },
+                        ),
+                    },
+                ),
+            },
+        )
+        "#);
+
+
+        let config = r#"
+        ports =
+            =
+            8000 =
+            8001 =
+            8002 =
+        "#;
+        let result = KeyVal::parse(config).unwrap();
+        insta::assert_debug_snapshot!(result, @r#"
+        [
+            KeyVal {
+                key: "ports",
+                value: "\n            =\n            8000 =\n            8001 =\n            8002 =",
+            },
+        ]
+        "#);
+        let ccl = CCL::parse(result);
+        insta::assert_debug_snapshot!(ccl, @r#"
+        CCL(
+            {
+                "ports": CCL(
+                    {
+                        "": CCL(
+                            {
+                                "": CCL(
+                                    {},
+                                ),
+                            },
+                        ),
+                        "8000": CCL(
+                            {
+                                "": CCL(
+                                    {},
+                                ),
+                            },
+                        ),
+                        "8001": CCL(
+                            {
+                                "": CCL(
+                                    {},
+                                ),
+                            },
+                        ),
+                        "8002": CCL(
+                            {
+                                "": CCL(
+                                    {},
+                                ),
+                            },
+                        ),
+                    },
+                ),
+            },
+        )
+        "#);
+
+        let config = r#"
+        ports
+            = 8000
+            = 8001
+            = 8002
+        "#;
+        let result = KeyVal::parse(config).unwrap();
+        insta::assert_debug_snapshot!(result, @r#"
+        [
+            KeyVal {
+                key: "ports",
+                value: "8000\n            = 8001\n            = 8002",
+            },
+        ]
+        "#);
+    }
+
 }
